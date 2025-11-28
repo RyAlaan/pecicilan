8f5a5c6926d81d179564a4e7dd5fc56ba15baf0e
Acidabest <rasheedkurniawan26@gmail.com>
2025-11-28 16:56:10 +0700

    detail page

--- js/script.js
+++ js/script.js
@@ -14,14 +14,13 @@ inputSearch.addEventListener("input", (e) => {
     request(`query=${encodeURIComponent(searchValue)}`);
   }, 600);
 });
-  
+
 const navbar = document.querySelector(".navbar");
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
