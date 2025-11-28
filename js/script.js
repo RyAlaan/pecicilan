607b469d5d3c16147242aca70b60ff9520e741f7
Acidabest <rasheedkurniawan26@gmail.com>
2025-11-28 20:54:17 +0700

    detail page

--- js/script.js
+++ js/script.js
@@ -50,12 +50,3 @@ inputSearch.addEventListener("keyup", (e) => {
   }, 600);
 });
 
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
