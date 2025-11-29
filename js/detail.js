b1a003984319614e074f9918812701c6e1c1e29a
ryalaan <alantisderry@gmail.com>
2025-11-29 07:59:25 +0700

    detailing

--- js/detail.js
+++ js/detail.js
@@ -1,7 +1,14 @@
 import { request } from "./api-service.js";
 
+const searchUrl =
+  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&";
+
 const url = "https://api.themoviedb.org/3/movie/";
+
 const navbar = document.querySelector(".navbar");
+const inputSearch = document.querySelector('input[name="searchInput"]');
+const suggestionList = document.querySelector(".suggestionList");
+
 const hero = document.querySelector(".hero");
 
 const heroTitle = document.querySelector(".hero-title");
@@ -23,6 +30,50 @@ window.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("scrolled");
   }
+});
+
+let searchTimeout;
+
+inputSearch.addEventListener("keyup", (e) => {
+  const searchValue = e.target.value;
+
+  clearTimeout(searchTimeout);
+
+  // call search value after 600ms of inactivity
+  searchTimeout = setTimeout(() => {
+    request(searchUrl + `query=${encodeURIComponent(searchValue)}`).then((data) => {
+      console.log(data);
+
+      suggestionList.innerHTML = "";
+
+      // mapping results
+      data.results.forEach((element) => {
+        console.log(element);
+
+        const suggestionItem = document.createElement("a");
+
+        suggestionItem.href = `./detail.html?id=${element.id}`;
+        suggestionItem.classList.add("suggestionItem");
+
+        const suggestionImage = document.createElement("img");
+
+        suggestionImage.src = `https://image.tmdb.org/t/p/original${element.poster_path}`;
+        suggestionImage.alt = element.title;
+        suggestionImage.classList.add("suggestionImage");
+
+        suggestionItem.appendChild(suggestionImage);
+
+        const suggestionTitle = document.createElement("p");
+
+        suggestionTitle.textContent = element.title;
+        suggestionTitle.classList.add("suggestionTitle");
+
+        suggestionItem.appendChild(suggestionTitle);
+
+        suggestionList.appendChild(suggestionItem);
+      });
+    });
+  }, 600);
 });
 
 const params = new URLSearchParams(window.location.search);
